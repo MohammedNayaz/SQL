bd74d7f10bd344ae97abf50909d0383aded70505
Mohammed Nayaz <64147100+MohammedNayaz@users.noreply.github.com>
2020-10-23 21:10:08 +0530

    Add files via upload

---
+++ Queries/The PADS.sql
@@ -0,0 +1,5 @@
+SELECT Name || '(' || SUBSTR(Occupation , 1 , 1) || ')'
+FROM OCCUPATIONS ORDER BY Name;
+
+SELECT 'There are a total of ' || COUNT(Name) || ' ' || LOWER(Occupation) || 's.'
+FROM Occupations GROUP BY Occupation ORDER BY COUNT(Name) , Occupation;
